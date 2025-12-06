ee7927e2bd202acc0d9c5758ddf76c0fa19e22cd
Sumathi <sumathi.sudhakar@gnapi.com>
2025-12-06 18:51:56 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import LandingPage from './components/LandingPage';
 import ProductList from './components/ProductList';
